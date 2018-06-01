62c4b4f486d07f961acd315e23da155665beba1f
xuguangmin <345253580@qq.com>
2018-06-01 18:56:55 +0800

    modify bug

--- ntpForEsp8266/main/user_main.c
+++ ntpForEsp8266/main/user_main.c
@@ -76,48 +76,37 @@ void ntp_begin(void *pvParameters)
 
 #if 1
 		/******* set system time ********/
-		// int settimeofday(const struct timeval *tv, const struct timezone *tz);
-		struct timeval tv;
+		struct timeval tv = {
+			.tv_sec = DS3231_Get_Time_Base1970(),
+			.tv_usec = 0
+		};
 
-		tv.tv_sec = DS3231_Get_Time_Base1970();
-		tv.tv_usec = 0;
 		if (settimeofday(&tv, NULL))
 			printf("settimeofday fail!\n");
 
 		if(gettimeofday(&tv, NULL))
 			printf("gettimeofday fail!!!\n");
-		//printf("gettimeofday  :%s", ctime(&tv.tv_sec));
-		/******* test ********/
+    	printf("Hello, welcome to NTP server. Current time: %s", ctime(&tv.tv_sec));
 #endif
 
-    printf("Hello, welcome to NTP server. Current time: %s", ctime(&tv.tv_sec));
-
 	while (1) {
-		memset(&ntpRecvMsg, '0', sizeof(NTP));
-		memset(&ntpSentMsg, '0', sizeof(NTP));
+		bzero(&ntpRecvMsg, sizeof(NTP));
+		bzero(&ntpSentMsg, sizeof(NTP));
 
 		if ((nbytes=recvfrom(fd, &ntpRecvMsg, sizeof(NTP), 0, (struct sockaddr *) &addr, (socklen_t *)&addrlen)) < 0) {
 			printf("recvfrom failed\r\n");
 		}
 
-
-		memset(&ntpSentMsg, '0', sizeof(NTP));
 		ntpSentMsg.LI_VN_Mode = htonl((LI_00<<30) | (VN<<27) | (MODE4<<24));
 		ntpSentMsg.stratum    = 4;	/* 对本地时钟级别的整体识别 */
 		ntpSentMsg.poll	= 1;	/* 测试间隔 */
 		ntpSentMsg.precision	= 0;	/* 本地时钟精度 */
 
-		
 		ntpSentMsg.reference_timestamp[0] = ntohl(DS3231_Get_Time_Base1900());
 		ntpSentMsg.orignate_timestamp[0]  = ntpRecvMsg.transmit_timestamp[0];
 		ntpSentMsg.orignate_timestamp[1]  = ntpRecvMsg.transmit_timestamp[1];
-		//ntpSentMsg.receive_timestamp[0]   = ntohl(DS3231_getTime_t());
-		//ntpSentMsg.transmit_timestamp[0]  = ntohl(DS3231_getTime_t());
-
 		ntpSentMsg.receive_timestamp[0]   = ntohl(DS3231_Get_Time_Base1900());
 		ntpSentMsg.transmit_timestamp[0]  = ntohl(DS3231_Get_Time_Base1900());
-
-
 
         if (sendto(fd, &ntpSentMsg, sizeof(NTP), 0, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
             printf("sendto failed\r\n");
@@ -134,19 +123,13 @@ void ntp_begin(void *pvParameters)
 		time_t rec_time = ntohl(ntpSentMsg.receive_timestamp[0]) - FROM1900TO1970 + SECONDS_8_HOUS;
 		time_t tra_time = ntohl(ntpSentMsg.transmit_timestamp[0])-FROM1900TO1970 + SECONDS_8_HOUS;
 
-		printf("reference_time:%d %s", ref_time, ctime(&ref_time));
-		printf("orignate_time :%d %s", ori_time, ctime(&ori_time));
-		printf("receive_time  :%d %s", rec_time, ctime(&rec_time));
-		printf("transmit_time :%d %s", tra_time, ctime(&tra_time));
-
+		printf("reference_time:%s", ctime(&ref_time));
+		printf("orignate_time :%s", ctime(&ori_time));
+		printf("receive_time  :%s", ctime(&rec_time));
+		printf("transmit_time :%s", ctime(&tra_time));
 		//drv_print_buff("ntpSentMsg", (char *)&ntpSentMsg, sizeof(ntpSentMsg));
 #endif
-		
-		bzero(&ntpRecvMsg, sizeof(NTP));
-		bzero(&ntpSentMsg, sizeof(NTP));
-
 	}
-    vTaskDelay(1);
 }
 
 /******************************************************************************
@@ -157,28 +140,35 @@ void ntp_begin(void *pvParameters)
  *******************************************************************************/
 void ntp_event_cb(System_Event_t *event)
 {
-    if (event == NULL) {
-        return;
-    }
-
-    switch (event->event_id) {
-        case EVENT_STAMODE_GOT_IP:
-            os_printf("sta got ip , creat fota task\n");
-            if (ntp_task_handle == NULL) {
-                xTaskCreate(ntp_begin, "ntp_task", 1024, NULL, 1, ntp_task_handle);
-            }
-            //os_timer_disarm(&upgrade_timer);
-            //os_timer_setfn(&upgrade_timer, (os_timer_func_t *) upgrade_recycle, NULL);
-            //os_timer_arm(&upgrade_timer, OTA_TIMEOUT, 0);
-            break;
-        case EVENT_SOFTAPMODE_STADISCONNECTED:
-            printf("sta disconnect from AP\n");
-            printf("recyle ota task");
-            //system_upgrade_flag_set(UPGRADE_FLAG_IDLE);
-            //upgrade_recycle();
-        default:
-            break;
-    }
+	if (event == NULL) {
+		return;
+	}
+	switch (event->event_id) {
+		case EVENT_STAMODE_GOT_IP:
+			os_printf("ntp got ip , creat NTP Server task\n");
+			if (ntp_task_handle == NULL) {
+				xTaskCreate(ntp_begin, "ntp_task", 1024, NULL, 1, ntp_task_handle);
+			}
+			break;
+		case EVENT_STAMODE_SCAN_DONE:        
+			os_printf("ntp scan done\n");
+			break;
+		case EVENT_STAMODE_CONNECTED:            
+			os_printf("ntp connected\n");
+			break;
+		case EVENT_STAMODE_DISCONNECTED:         
+			os_printf("ntp disconnected\n");
+			system_restart();
+			break;
+		case EVENT_STAMODE_AUTHMODE_CHANGE:      
+			os_printf("ntp authmode change\n");
+			break;
+		case EVENT_STAMODE_DHCP_TIMEOUT:         
+			os_printf("ntp dhcp timeout\n");
+			break;
+		default:
+			break;
+	}
 }
 
 /******************************************************************************
